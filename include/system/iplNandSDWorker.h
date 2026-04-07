39eec24f3061658f4229a078fd04dfcf553b22d8
Tanline666 <93364055+Tanline666@users.noreply.github.com>
2026-04-07 17:52:02 +0000

    Update iplNandSDWorker.h

--- include/system/iplNandSDWorker.h
+++ include/system/iplNandSDWorker.h
@@ -2,6 +2,7 @@
 #define IPL_NAND_SD_WORKER_H
 
 #include <revolution.h>
+#include "revolution/net/NETDigest.h"
 
 namespace ipl {
     class NandSDWorker {
@@ -14,8 +15,12 @@ namespace ipl {
          * @brief Verifies if the banner header is valid.
          * @param pBnrData The buffer containing the banner data.
          * @param headerSize Header file size.
+         * @param sum
+         * @param buffer
+         * @param length
          */
         BOOL check_header_base(const u8* bnrData, u32* headerSize = NULL);
+        BOOL check_md5(const u8* sum, const u8* buffer, u32 length);
     };
 }  // namespace ipl
 
